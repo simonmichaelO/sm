f15456fb369518c6bd9f3bb79bd04698dc60346a
Simon Michael <112586912+simonmichaelO@users.noreply.github.com>
2024-06-19 16:53:56 +0100

    Update script.js 2

--- script.js
+++ script.js
@@ -138,3 +138,17 @@ function scrollActive() {
 
 window.addEventListener("scroll", scrollActive);
 
+
+// contact form 
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxJOaUO4zBEbCLgD3u9E06v9FiszSUvEvrYLwqGMZwHHlLGXTqfctT24D08IhrkrH4/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
